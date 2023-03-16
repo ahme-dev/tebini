8623ee7e11a42c399f361ec52d51d35381239b3c
Ahmed K.A <ahmadkabdullah@pm.me>
2023-03-16 12:23:10 +0300

    Add solid template and tailwind with sorter

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import type { Component } from "solid-js";
+
+const App: Component = () => {
+	return (
+		<div>
+			<h1 class="p-8 text-fuchsia-400">Hello</h1>
+			<h3></h3>
+		</div>
+	);
+};
+
+export default App;
