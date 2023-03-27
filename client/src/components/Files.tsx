d9282e8e548de0228d5eae85fe303742a4bb8072
Ahmed K.A <ahmadkabdullah@pm.me>
2023-03-27 14:35:10 +0300

    Add page and book adding elements displaying on hover

--- client/src/components/Files.tsx
+++ client/src/components/Files.tsx
@@ -1,11 +1,17 @@
+import { FiPlusCircle } from "solid-icons/fi";
 import { produce } from "solid-js/store";
 import { notes, setNotes } from "../store";
 import { FilesItem } from "./FilesItem";
 
 export function Files() {
 	return (
-		<div class="flex h-fit flex-col bg-zinc-700/40">
-			<p class="px-4 py-2">Files</p>
+		<div class="group flex h-fit flex-col bg-zinc-700/40">
+			<div class="flex flex-row items-center justify-between px-4 py-2">
+				<p>Files</p>
+				<div class="opacity-0 transition-all hover:cursor-pointer group-hover:opacity-100">
+					<FiPlusCircle />
+				</div>
+			</div>
 			{notes.books.map((book, bookIndex) => {
 				return (
 					<>
--- client/src/components/FilesItem.tsx
+++ client/src/components/FilesItem.tsx
@@ -1,4 +1,4 @@
-import { FiFile, FiFolder } from "solid-icons/fi";
+import { FiFile, FiFilePlus, FiFolder } from "solid-icons/fi";
 
 export function FilesItem(props: {
 	title: string;
@@ -9,14 +9,21 @@ export function FilesItem(props: {
 	return (
 		<div
 			onclick={props.handleClick}
-			class={`flex h-fit flex-col gap-2 px-4 py-1 hover:cursor-pointer hover:bg-zinc-600/40
+			class={`group/dir flex h-fit flex-col gap-2 px-4 py-1 hover:bg-zinc-600/40
 				${props.isSelected && "border border-zinc-500 bg-zinc-600/40"}
-				${!props.isDir && "pl-8"}
+				${!props.isDir && "pl-8 hover:cursor-pointer"}
 			`}
 		>
-			<div class="flex items-center gap-2 text-sm font-bold">
-				{props.isDir ? <FiFolder /> : <FiFile />}
-				<p>{props.title}</p>
+			<div class="flex items-center justify-between gap-2 text-sm font-bold">
+				<div class="flex items-center gap-2">
+					{props.isDir ? <FiFolder /> : <FiFile />}
+					<p>{props.title}</p>
+				</div>
+				{props.isDir && (
+					<div class="opacity-0 transition-all hover:cursor-pointer group-hover/dir:opacity-100">
+						<FiFilePlus />
+					</div>
+				)}
 			</div>
 		</div>
 	);
