b01a08821512d274d20f3ebeb84007477d79b846
Ahmed K.A <ahmadkabdullah@pm.me>
2023-03-27 13:46:02 +0300

    Make input resizable

--- client/src/components/Note.tsx
+++ client/src/components/Note.tsx
@@ -2,7 +2,7 @@ import { NoteInput } from "./NoteInput";
 
 export function Note() {
 	return (
-		<div class="flex grow flex-col gap-4 p-2">
+		<div class="flex w-8 grow flex-col gap-4 p-2">
 			<NoteInput></NoteInput>
 			<NoteInput isMultiline></NoteInput>
 		</div>
