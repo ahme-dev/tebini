2f4b40b0de1538a6100dab11b3af6e506bbae4d8
Ahmed K. A <ahmadkabdullah@pm.me>
2023-04-09 20:01:29 +0300

    Add page editor component and tie to store

--- client/src/App.vue
+++ client/src/App.vue
@@ -1,25 +1,43 @@
 <script setup lang="ts">
+import PageEditorVue from "./components/PageEditor.vue";
 import { useNotesStore } from "./stores/notes";
 
 const store = useNotesStore();
+("bg-transparent py-2 border-l-2 border-l-transparent focus:(outline-none ring-none px-4 border-l-amber-3) transition-border transition-padding");
 </script>
 
 <template>
-	<div class="p-4 flex flex-col gap-2 bg-gray-9 h-screen text-gray-3">
-		<p>Current: {{ store.current }}</p>
-		<div
-			v-for="page in store.pages"
-			:key="page.id"
-			class="bg-blue-9 cursor-pointer p-2 rounded-4"
-			:class="[page.id === store.current ? 'border-red-5' : 'border-transparent', 'border-2']"
-			@click="() => store.setCurrent(page.id)"
-		>
-			<h1>{{ page.title }}</h1>
-			<p>{{ page.parent }}</p>
-			<button class="border" @click="store.trashPage(page.id)" v-if="!page.inTrash">
-				Trash it
-			</button>
-			<button class="border" @click="store.restorePage(page.id)" v-else>Restore</button>
+	<div class="min-h-screen bg-zinc-9 text-neutral-1 flex flex-col-reverse p-4 md:(flex-row p-8)">
+		<!-- left section -->
+		<div class="grow flex flex-col">
+			<PageEditorVue></PageEditorVue>
 		</div>
+		<!-- left section end -->
+
+		<!-- right section -->
+		<div>
+			<!-- explorer -->
+			<div class="flex flex-col gap-2 text-gray-3">
+				<p>Current: {{ store.current }}</p>
+				<div
+					v-for="page in store.pages"
+					:key="page.id"
+					class="bg-blue-9 cursor-pointer p-2 rounded-4"
+					:class="[page.id === store.current ? 'border-red-5' : 'border-transparent', 'border-2']"
+					@click="() => store.setCurrent(page.id)"
+				>
+					<h1>{{ page.title }}</h1>
+					<p>{{ page.parent }}</p>
+					<button class="border" @click="store.trashPage(page.id)" v-if="!page.inTrash">
+						Trash it
+					</button>
+					<button class="border" @click="store.restorePage(page.id)" v-else>Restore</button>
+				</div>
+			</div>
+
+			<!-- icons -->
+			<div></div>
+		</div>
+		<!-- right section end -->
 	</div>
 </template>
---
+++ client/src/components/PageEditor.vue
@@ -0,0 +1,21 @@
+<script lang="ts" setup>
+import { useNotesStore } from "@/stores/notes";
+
+const store = useNotesStore();
+
+const inputStyles =
+	"bg-transparent py-2 border-l-2 border-l-transparent focus:(outline-none ring-none px-4 border-l-amber-3) transition-border transition-padding";
+</script>
+
+<template>
+	<div class="grow flex flex-col">
+		<input
+			:class="inputStyles"
+			v-model="store.getCurrent!.title"
+			class="font-bold text-xl"
+			type="text"
+			placeholder="title"
+		/>
+		<textarea :class="inputStyles" class="grow" placeholder="content" />
+	</div>
+</template>
