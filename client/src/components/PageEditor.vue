9c9a095066a8a13a9bbc1e4da2f4290ffb94f39b
Ahmed K. A <ahmadkabdullah@pm.me>
2023-04-13 21:28:50 +0300

    Add ability to change book title for pages

--- client/src/components/PageEditor.vue
+++ client/src/components/PageEditor.vue
@@ -1,8 +1,11 @@
 <script lang="ts" setup>
 import { useNotesStore } from "@/stores/notes";
 import PageEditorItem from "./PageEditorItem.vue";
+import { ref } from "vue";
 
 const store = useNotesStore();
+
+const changeBookShown = ref(false);
 
 const inputStyles =
 	"bg-transparent py-2 border-l-2 border-l-transparent focus:(outline-none ring-none px-4 border-l-amber-3) transition-border transition-padding";
@@ -20,6 +23,22 @@ const inputStyles =
 
 		<!-- status items -->
 		<div class="flex flex-row gap-2 text-sm">
+			<div class="relative cursor-pointer">
+				<PageEditorItem
+					@click="changeBookShown = !changeBookShown"
+					label="book"
+					icon="i-mdi-book"
+					:value="store.getCurrent!.book"
+				/>
+				<input
+					@focusout="changeBookShown = false"
+					v-if="changeBookShown"
+					class="absolute font-bold top-0 z-10 left-4 focus:(outline-none) bg-amber-3 px-1 text-neutral-9 w-24 border-r-4 border-neutral-9"
+					v-model="store.getCurrent!.book"
+					type="text"
+				/>
+			</div>
+
 			<PageEditorItem
 				label="creation"
 				icon="i-mdi-clock-check-outline"
