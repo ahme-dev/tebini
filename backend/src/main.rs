0fca942237a238fd52330ba8de9108a16b74b0ab
Ahmed K. A <ahmadkabdullah@pm.me>
2023-04-14 21:29:02 +0300

    Separate router for hello

--- backend/src/main.rs
+++ backend/src/main.rs
@@ -8,17 +8,22 @@ use serde::Deserialize;
 
 #[tokio::main]
 async fn main() {
-    let routes = Router::new()
-        .route("/hello", get(hello_controller))
-        .route("/hello/:name", get(hello2_controller));
+    let router = Router::new().merge(hello_router());
 
     // start server
     let addr = SocketAddr::from(([127, 0, 0, 1], 3000));
     println!("Listening on http://{}/", addr);
     axum::Server::bind(&addr)
-        .serve(routes.into_make_service())
+        .serve(router.into_make_service())
         .await
         .unwrap();
+}
+
+// router for hello routes
+fn hello_router() -> Router {
+    Router::new()
+        .route("/hello", get(hello_controller))
+        .route("/hello/:name", get(hello2_controller))
 }
 
 #[derive(Debug, Deserialize)]
