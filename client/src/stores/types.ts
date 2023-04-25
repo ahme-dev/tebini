be0e788784d6845f3e412b5283241ada5686c17d
Ahmed K. A <ahmadkabdullah@pm.me>
2023-04-25 13:26:51 +0300

    Clear out the project completely

--- client/src/stores/types.ts
+++
@@ -1,8 +0,0 @@
-export interface Page {
-	id: string;
-	title: string;
-	content: string;
-	book: string;
-	inTrash: boolean;
-	createdAt: string;
-}
--- client/uno.config.ts
+++
@@ -1,6 +0,0 @@
-import { defineConfig, presetIcons, presetWind, transformerVariantGroup } from "unocss";
-
-export default defineConfig({
-	presets: [presetWind(), presetIcons()],
-	transformers: [transformerVariantGroup()],
-});
