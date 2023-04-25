9d058bc0cb1d6f272055e28bea4f64654aba6472
Ahmed K. A <ahmadkabdullah@pm.me>
2023-04-25 14:49:30 +0300

    Change some default settings and clear README file

--- prettier.config.cjs
+++ prettier.config.cjs
@@ -1,6 +1,10 @@
 /** @type {import("prettier").Config} */
 const config = {
-  plugins: [require.resolve("prettier-plugin-tailwindcss")],
+	plugins: [require.resolve("prettier-plugin-tailwindcss")],
+	useTabs: true,
+	semi: true,
+	trailingComma: "all",
+	singleQuote: false,
 };
 
 module.exports = config;
